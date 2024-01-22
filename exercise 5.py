49823d8e3355bf55c72ad11be737d75895be391c
johnmichaelpplale <johnmichaelpplale@gmail.com>
2024-01-22 11:31:53 +0800

    printing the first given numbers

--- exercise 5.py
+++ exercise 5.py
@@ -11,4 +11,9 @@ def similarity_checker(numbers):
     
 # declare a list of number
 numbers_x = [10, 20, 30, 40, 10]
-numbers_y = [75, 65, 35, 75, 30]        
+numbers_y = [75, 65, 35, 75, 30]   
+
+# print the numbers_x given
+print(f"Given lists: {numbers_x}")
+print("result is", similarity_checker(numbers_x))
+
